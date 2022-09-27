e1ff416a4d6329df6db029519ecd04455a861ee9
Long Nguyen <56315912+Long1908@users.noreply.github.com>
2022-09-27 07:06:57 +0300

    Update index.js

--- Drum Kit FInished Project/index.js
+++ Drum Kit FInished Project/index.js
@@ -24,10 +24,23 @@ function makeSound(key) {
             audio = new Audio("sounds/crash.mp3");
         break;
         default:
-            alert("NO drum!");
         break;
     }
     audio.play();
+}
+function buttonAnimation(key) {
+    var currentActiveKey = document.querySelector("." + key); // key has a right now a single letter value, so if want to
+    //specify the class we are working with, we need to concatinate with a dot in front
+    currentActiveKey.classList.add("pressed"); /*We are adding the class
+    'pressed' to give this sense of animation in our drub website. Notice how when we specify, what
+    class we want to add to the button, we don't put dot in front in this case, when we call the 
+    function 'add'*/
+    setTimeout(function() {
+       currentActiveKey.classList.remove("pressed");
+    }, 100); /*If we only added pressed class to a button, then the corresponding button will 
+    always stay pressed. So to complete our animation, we call a special method called setTimeout,
+    where its' input are a function to call, and a timer in which it will call the function, after
+    the timer has ran out(in mili-seconds)*/
 }
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;//This works and it will give us
 //the number of elements with the same class name. Important that querySelectorAll works as array 
@@ -38,11 +51,15 @@ var audio;// https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/A
 for(var i = 0; i < numberOfDrumButtons; i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        makeSound(this.innerHTML);
+        makeSound(this.innerHTML);//this.innerHTML will return the text of the button we clicked
+        buttonAnimation(this.innerHTML);
     });
+    
 }
 
 //Detecting keyboard press:
 document.addEventListener("keydown", function(event) {
-    makeSound(event.key);
+    makeSound(event.key);//event.key will return the letter of the corresponding key
+    buttonAnimation(event.key);
 });
+
